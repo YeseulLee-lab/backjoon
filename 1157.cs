3abadf08425cb7b226b1cbb83a4cf310f5573d6b
YesMan <kaka0804@naver.com>
2025-07-26 21:41:33 +0900

    Feat: 백준 1157 단어공부

---
+++ 1157.cs
@@ -0,0 +1,41 @@
+﻿public class Example
+{
+    public static void Main(string[] args)
+    {
+        var sr = new StreamReader(Console.OpenStandardInput());
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        string str = sr.ReadLine();
+        str = str.ToLower();
+
+        int[] chars = new int[26];
+
+        for (int i = 0; i < 26; i++)
+        {
+            chars[i] = 0;
+        }
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            chars[str[i] - 'a']++;
+        }
+
+        string result = "";
+        int max = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            if (chars[i] > max)
+            {
+                max = chars[i];
+                result = ((char)(i + 'A')).ToString();
+            }
+            else if(chars[i] == max)
+            {
+                result = "?";
+            }
+        }
+
+        sw.WriteLine(result);
+        sw.Flush();
+    }
+}
