ffc7bb69c263e6eb97ade9b5ec7bb22604b4ef1a
YesMan <kaka0804@naver.com>
2025-07-26 21:38:53 +0900

    Feat: 백준 10807 개수 세기

---
+++ 10807.cs
@@ -0,0 +1,27 @@
+﻿public class Example
+{
+    public static void Main(string[] args)
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+        var sr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(sr.ReadLine());
+
+        string str = sr.ReadLine();
+        string[] newstr = str.Split(" ");
+
+        int v = int.Parse(sr.ReadLine());
+
+        int result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (int.Parse(newstr[i].ToString()) == v)
+            {
+                result++;
+            }
+        }
+
+        sw.WriteLine(result);
+        sw.Flush();
+    }
+}
