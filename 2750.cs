23445ecf1e52eb8440e3196d1299ced458fd52be
YesMan <kaka0804@naver.com>
2025-07-26 21:41:58 +0900

    Feat: 백준 2750 수 정렬하기

---
+++ 2750.cs
@@ -0,0 +1,25 @@
+﻿public class Example
+{
+    public static void Main(string[] args)
+    {
+        var sr = new StreamReader(Console.OpenStandardInput());
+        var sw = new StreamWriter(Console.OpenStandardOutput());
+
+        int n = int.Parse(sr.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(sr.ReadLine());
+        }
+
+        Array.Sort(arr);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sw.WriteLine(arr[i]);
+            sw.Flush();
+        }
+    }
+}
